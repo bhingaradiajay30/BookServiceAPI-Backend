e35716c1086a37c5f7f0c868777d5b27c123314a
bhingaradiajay30 <bhingaradia.jay30@gmail.com>
2023-09-24 22:42:18 -0400

    Service-API-Backend

---
+++ src/main/java/com/Jaykumar/bookservice/Entity/Book.java
@@ -0,0 +1,76 @@
+package com.Jaykumar.bookservice.Entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity 
+public class Book {
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String author;
+    
+    @Column(name = "\"year\"")
+    private Integer year;
+
+    // No-argument constructor
+    public Book() {
+    }
+
+    // Constructor with parameters
+    public Book(Long id, String title, String author, Integer year) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    // Getter and setter methods
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    // toString method
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", year=" + year +
+                '}';
+    }
+}
